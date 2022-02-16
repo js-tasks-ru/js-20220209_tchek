adbd80fc0253edfe1c91853d454435d233ead0ef
Александр Ч <birth25@yandex.ru>
2022-02-16 23:27:25 +0300

    Task 3. 1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,24 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const delimiter = '.';
 
+  const selector = (obj, pathSplited = path) => {
+    if (obj === undefined) {
+      return undefined;
+    }
+
+    const firstDelimiterIndex = pathSplited.indexOf(delimiter);
+
+    if (firstDelimiterIndex === -1) {
+      return obj[pathSplited];
+    }
+
+    const first = pathSplited.slice(0, firstDelimiterIndex);
+    const rest = pathSplited.slice(firstDelimiterIndex + 1);
+
+    return selector(obj[first], rest);
+  }
+
+  return selector;
 }
